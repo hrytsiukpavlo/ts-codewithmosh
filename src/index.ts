d74aea38e777ed02ec1180c03ad8654b5d066407
hrytsiukpavlo <overdrive800392@gmail.com>
2022-11-04 15:55:58 +0200

    section 3 finished
    section 3 finished

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,29 @@
-let age: number = 20;
-if (age < 50) {
-	age += 10;
-	console.log(age);
-}
+//For each of these values, what type will the TypeScript compiler infer?
+//Number
+let a = 100;
+//String
+let b = "Coffee";
+//Boolean[]
+let c = [true, false, false];
+//{age: 20}
+let d = { age: 20 };
+//Number[]
+let e = [3];
+//Any
+let f;
+//Any[]
+let g = [];
+
+//What are the compilation errors in each of the following code snippets?
+//No releaseYear
+// let song: {
+// 	title: string,
+// 	releaseYear: number
+//  } = { title: 'My song' };
+
+//Elements of array should be numbers
+// let prices = [100, 200, 300];
+// prices[0] = '$100';
+
+//Function returns void instead of number
+// function myFunc(a: number, b: number): number { }
