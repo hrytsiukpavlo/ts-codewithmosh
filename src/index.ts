88721b8373679cc3f68873d7426fd50cb7910e41
hrytsiukpavlo <overdrive800392@gmail.com>
2022-11-06 13:48:33 +0200

    section 6 finished
    section 6 finished

--- src/index.ts
+++ src/index.ts
@@ -1,54 +1,26 @@
-//Define a class called Logger that takes the name of a file in its constructor and provides
-//a method for writing messages to that file. Don’t worry about the actual file I/O
-//operations. Just define the class with the right members.
-
-class Logger {
-	name: string;
-	constructor(name: string) {
-		this.name = name;
-	}
-
-	log(message: string) {}
+//Convert the function below to a generic function:
+function echo<T>(arg: T) {
+	return arg;
 }
 
-//Given the Person class below, create a getter for getting the full name of a person
-class Person {
-	constructor(public firstName: string, public lastName: string) {}
-
-	get fullname() {
-		return `${this.firstName} ${this.lastName}`;
-	}
+//When compiling the following piece of code, we get an error saying ‘Property name
+// does not exist on type T’. How can we solve this problem?
+function printName<T extends { name: string }>(obj: T) {
+	console.log(obj.name);
 }
 
-//Create a new class called Employee that extends Person and adds a new property
-//called salary.
-class Employee extends Person {
-	constructor(firstName: string, lastName: string, public salary: number) {
-		super(firstName, lastName);
-	}
+//An Entity should have a unique identifier. The type of identifier, however, is dependent
+// on the use case. In some cases, the ID might be a number, in other cases, it might be a
+// string, GUID, etc. Represent the entity using a generic class.
+
+class Entity<T> {
+	constructor(public id: T) {}
 }
 
-//What is the difference between private and protected members?
-//Protected is availiable in children
-
-//Given the data below, define an interface for representing employees:
-
-// let employee = {
-// 	name: 'John Smith',
-// 	salary: 50_000,
-// 	address: {
-// 	street: 'Flinders st',
-// 	city: 'Melbourne',
-// 	zipCode: 3144,
-// 	},
-//  };
-
-interface Employee {
-	name: string;
-	salary: number;
-	address: {
-		street: string;
-		city: string;
-		zipCode: number;
-	};
+//Given the following interface, what does keyof User return?
+interface User {
+	userId: number;
+	username: string;
 }
+
+//userId | username
